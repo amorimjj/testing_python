7a2b76ed88d3fdbb42180b47f47c9a26b0f13ea3
Jeferson Amorim <amorimjj@gmail.com>
2025-12-03 18:10:31 -0300

    Improve lists

--- src/list.py
+++ src/list.py
@@ -5,6 +5,9 @@ print(my_list[2])
 
 my_dict = { 'key1': "abc", 'key2': "dfg" }
 
+my_set1 = { 1, 2, 3, 4}
+my_set2 = { 4, 5, 6, 7}
+
 if 'key3' in my_dict:
     print('all good')
 else:
@@ -13,3 +16,12 @@ else:
 name = "Alice"
 
 print("Hello, {1} {0}!".format("sei", name))
+
+print(my_set1)
+print(my_set1 | my_set2)
+print(my_set1 & my_set2)
+print(my_set1 - my_set2)
+print(my_set1 ^ my_set2)
+
+for item in my_set1:
+    print(item)
